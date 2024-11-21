b58f04a3cadea2be8fbf93373097739c3304664f
Jailin <jailinalbornoz@gmail.com>
2024-11-21 16:11:40 -0300

    Delete ejercicio_01 directory

--- ejercicio_01/clases/__init__.py
+++
@@ -1,5 +0,0 @@
-#__init__.py de la carpeta clases
-from .dinosaurioclass import dinosaurioclass
-from .carnivoroclass import carnivoroclass
-from .herbivoroclass import herbivoroclass
-from .omnivoroclass import omnivoroclass
