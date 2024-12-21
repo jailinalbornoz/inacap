22d6bd34e8a9cf8320e3cad1594551fa0607c62c
Jailin <jailinalbornoz@gmail.com>
2024-12-21 18:00:40 -0300

    Delete ejercicio_01 directory

--- ejercicio_01/main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from metodos.menu import main
-from metodos.ingresos import ingresar_carnivoro,ingresar_herbivoro, ingresar_omnivoro
-from metodos.interacciones import actualizar_revision, revisiones_atrasadas, mostrar_dinosaurios
-
-if __name__ == "__main__":
-    main()
--- ejercicio_01/metodos/__init__.py
+++
@@ -1,4 +0,0 @@
-#__init__.py de la carpeta metodos
-from .ingresos import ingresar_carnivoro,ingresar_herbivoro, ingresar_omnivoro
-from .interacciones import actualizar_revision, revisiones_atrasadas, mostrar_dinosaurios
-from .menu import main, mostrar_menu
